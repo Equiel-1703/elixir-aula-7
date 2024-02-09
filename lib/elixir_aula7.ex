79eb50cf6fe622d7d71b4728cb3567ef0c1592d6
Henrique Rodrigues <henrique.grdr@inf.ufpel.edu.br>
2024-02-09 10:46:38 -0300

    Initial commit

---
+++ lib/elixir_aula7.ex
@@ -0,0 +1,18 @@
+defmodule ElixirAula7 do
+  @moduledoc """
+  Documentation for `ElixirAula7`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirAula7.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/elixir_aula7_test.exs
@@ -0,0 +1,8 @@
+defmodule ElixirAula7Test do
+  use ExUnit.Case
+  doctest ElixirAula7
+
+  test "greets the world" do
+    assert ElixirAula7.hello() == :world
+  end
+end
